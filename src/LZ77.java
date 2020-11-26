8a500f0251281fcd206280e7eab3fbbba988ffb2
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2020-11-26 21:55:16 +0200

    Making boundary rules for search buffer and look ahead window

--- src/LZ77.java
+++ src/LZ77.java
@@ -5,7 +5,8 @@ public class LZ77 {
 	private static String data = "";
 	private static Tag tag;
 	private static ArrayList<Tag> allTags = new ArrayList<Tag>();
-	private static int lookAheadWinStart = 0;
+	private static int searchBuffStart = 0, searchBuffEnd = 0;
+	private static int lookAheadWinStart = 0, lookAheadWinEnd = 0;
 	
 	// constants
 	private static int SEARCH_BUFF_SIZE = 7, LOOK_AHEAD_WIN_SIZE = 7;
@@ -28,6 +29,22 @@ public class LZ77 {
 		// while the algorithm still working on data compression
 		while(lookAheadWinStart < data.length()) {
 			
+			// updating the search buffer and the look ahead window scope
+			searchBuffEnd = lookAheadWinStart; // always attached
+			
+			if(searchBuffEnd < 7) // search buffer start calculation
+				searchBuffStart = 0;
+			else
+				searchBuffStart = searchBuffEnd - SEARCH_BUFF_SIZE;
+			
+			if(lookAheadWinStart + 7 >= data.length()) // search buffer end boundary
+				lookAheadWinEnd = data.length();
+			else
+				lookAheadWinEnd = lookAheadWinStart + LOOK_AHEAD_WIN_SIZE;
+			
+			System.out.println(data.substring(lookAheadWinStart, lookAheadWinEnd));
+			
+			lookAheadWinStart++;
 		}
 	}
 
