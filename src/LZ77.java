d24763098fa9896c509936d6b80ca322df410873
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2020-11-26 20:39:37 +0200

    Defining array of Tags and printing all of them

--- src/LZ77.java
+++ src/LZ77.java
@@ -1,17 +1,38 @@
+import java.util.ArrayList;
 
 public class LZ77 {
 	
 	private static String data = "";
+	private static Tag tag;
+	private static ArrayList<Tag> allTags = new ArrayList<Tag>();
 	
 	LZ77(String d){
 		data = d;
 	}
+	
+	private void printAllTags() {
+		for(int i = 0; i < allTags.size(); i++) {
+			allTags.get(i).printTag();
+			System.out.print(" ");
+		}
+		
+		System.out.print("\n");
+	}
 
 	public void compress() {
-		// Code for compression
+		tag = new Tag(1, 2, 'A');
+		allTags.add(tag);
+		
+		tag = new Tag(2, 4, 'B');
+		allTags.add(tag);
+		
+		tag = new Tag(8, 10, 'X');
+		allTags.add(tag);
+		
+		printAllTags();
 	}
 
 	public void decompress() {
-		// Code for decompression
+		System.out.println("Original data: " + data + "!");
 	}
 }
